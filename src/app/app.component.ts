7c6540749886b67e5989e8df918159101f3a4d1b
IhebGarsii <ihebgarsi78@gmail.com>
2024-03-09 13:17:38 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'luffy';
+  card = {
+    imageURL: '',
+    power: 0,
+    name: '',
+  };
+  cards: any[] = [];
+
+  add() {
+    this.cards.push(this.card);
+    this.card = {
+      imageURL: '',
+      power: 0,
+      name: '',
+    };
+    console.log(this.cards);
+  }
 }
